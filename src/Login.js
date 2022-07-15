6bcaf31db23b0064bfe28b245f1c0476369514aa
Carlos Arbizu <carlos2345532@gmail.com>
2022-07-14 21:10:42 -0500

    user registration finished

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import amazonLogoSignIn from './images/amazonLogoSignIn.png'
 import {Link} from 'react-router-dom';
 import './Login.css';
+import {auth} from './firebase';
 function Login() {
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+
+    const signIn = (e) => {
+        
+        e.preventDefault();
+    }
+    const register = (e) => {
+        auth.createUserWithEmailAndPassword(
+            email,password
+        ).then((authObj) => {
+            console.log(authObj);
+        }).catch(error => alert(error.message));
+    }
+
     return (
         <div id='login'>
             <div id="loginContainer">
@@ -11,10 +27,10 @@ function Login() {
                 <fieldset id="loginFieldSet">
                 <h1>Sign-In</h1>
                 <label for="email"><strong>Email:</strong></label>
-                <input type="email" name="email" id="email"/>
+                <input type="email" value={email} onChange= {e => setEmail(e.target.value)} name="email" id="email"/>
                 <label for="password"><strong>Password:</strong></label>
-                <input type="password" name="password" id="password"/>
-                <input type="submit" value="Continue" />
+                <input type="password" value={password} onChange={e=> setPassword(e.target.value)} name="password" id="password"/>
+                <input onClick={signIn} type="submit" value="Continue" />
                 <p>By continuing, you agree to Amazon's <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Conditions of Use</a> and <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"> Privacy Notice.</a></p>
                 </fieldset>
             </form>
@@ -22,7 +38,7 @@ function Login() {
             <div id="divider">
             <p id="newUserText">New to Amazon?</p>
             </div>
-            <button id="createAccount">Create your Amazon Account</button>
+            <button onClick={register} id="createAccount">Create your Amazon Account</button>
             </div>
             <footer>
                 <p>© Crypto-dot</p>
